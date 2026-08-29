5a90ab04f66df7fe66bbd7f6ed7f98653793521e
niknikdym-hue <niknikdym@gmail.com>
2026-08-29 16:39:23 +0300

    Compile Dilon native flow controller

--- audiobook-studio/native/build_native_app.sh
+++ audiobook-studio/native/build_native_app.sh
@@ -31,6 +31,7 @@ xcrun swiftc \
   "$script_dir/StudioContracts.swift" \
   "$script_dir/AudioQAContracts.swift" \
   "$script_dir/EmbeddedAudioPlayer.swift" \
+  "$script_dir/DilonNativeFlowController.swift" \
   "$script_dir/DilonNativeCard.swift" \
   "$script_dir/AudiobookStudioApp.swift" \
   -parse-as-library \
